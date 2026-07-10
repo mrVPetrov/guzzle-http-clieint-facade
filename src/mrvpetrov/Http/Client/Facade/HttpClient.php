904746c460f77a4a90e265e97484c9a2f926c859
vadim <vadimopetrov@bk.ru>
2026-07-11 01:26:44 +0500

    feat(http client): get and post methods added. Options parameter added to constructor

--- src/mrvpetrov/Http/Client/Facade/HttpClient.php
+++ src/mrvpetrov/Http/Client/Facade/HttpClient.php
@@ -1,5 +1,6 @@
 <?php
 	namespace mrvpetrov\Http\Client\Facade;
+
 	use GuzzleHttp\Client;
 	use GuzzleHttp\Exception\GuzzleException;
 	use mrvpetrov\Http\HttpMethodEnum;
@@ -8,14 +9,22 @@
 
 	class HttpClient {
 		private Client $_client;
-		public function __construct() {
-			$this->_client = new Client();
+		public function __construct(?HttpClientOptions $options = null) {
+			$this->_client = new Client($options?->asArray() ?? []);
 		}
 
-		/**
-		 * @throws GuzzleException
-		 */
-		public function request(HttpMethodEnum $method, string $url, HttpRequestOptions $options): ResponseInterface {
-			return $this->_client->request($method->value, $url, $options->getOptionsAsArray());
+		/** @throws GuzzleException */
+		public function request(HttpMethodEnum $method, string $url, ?HttpRequestOptions $options = null): ResponseInterface {
+			return $this->_client->request($method->value, $url, $options?->getOptionsAsArray() ?? []);
+		}
+
+		/** @throws GuzzleException */
+		public function get(string $url, ?HttpRequestOptions $options = null): ResponseInterface {
+			return $this->_client->get($url, $options?->getOptionsAsArray() ?? []);
+		}
+
+		/** @throws GuzzleException */
+		public function post(string $url, ?HttpRequestOptions $options = null): ResponseInterface {
+			return $this->_client->post($url, $options?->getOptionsAsArray() ?? []);
 		}
 	}
