feed4ff1c8220318367e514b06af4f6747bcbf03
vadim <vadimopetrov@bk.ru>
2026-07-11 01:52:28 +0500

    fix(http request header): constructor with parameters added

--- src/mrvpetrov/Http/Client/Facade/HttpHeader.php
+++ src/mrvpetrov/Http/Client/Facade/HttpHeader.php
@@ -5,4 +5,9 @@
 	class HttpHeader {
 		public string $name;
 		public string|array $value;
+
+		public function __construct(string $name, string|array $value) {
+			$this->name = $name;
+			$this->value = $value;
+		}
 	}
--- test/mrvpetrov/Http/Client/Facade/HttpClientTest.php
+++ test/mrvpetrov/Http/Client/Facade/HttpClientTest.php
@@ -4,15 +4,16 @@
 
 	use mrvpetrov\Http\Client\Facade\HttpClient;
 	use mrvpetrov\Http\Client\Facade\HttpClientOptions;
+	use mrvpetrov\Http\Client\Facade\HttpHeader;
+	use mrvpetrov\Http\Client\Facade\HttpHeaderList;
 	use PHPUnit\Framework\TestCase;
 	use function PHPUnit\Framework\assertEquals;
 
 	class HttpClientTest extends TestCase {
 		function test_request() {
-			$client = new HttpClient(
-				HttpClientOptions::new()
-					->setBaseUri('https://jsonplaceholder.typicode.com/')
-			);
+			$options = HttpClientOptions::new()->setBaseUri('https://jsonplaceholder.typicode.com/');
+			$options->headers = new HttpHeaderList([new HttpHeader("test", 'value')]);
+			$client = new HttpClient($options);
 
 			$res = $client->get( 'posts');
 
