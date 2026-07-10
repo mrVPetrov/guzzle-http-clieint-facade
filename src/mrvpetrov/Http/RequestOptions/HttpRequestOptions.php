4068c56739ef4123996ae84c79357fa9e1a8f1af
vadim <vadimopetrov@bk.ru>
2026-07-11 01:23:52 +0500

    fix: removed some request options

--- src/mrvpetrov/Http/RequestOptions/HttpRequestOptions.php
+++ src/mrvpetrov/Http/RequestOptions/HttpRequestOptions.php
@@ -14,15 +14,11 @@
 
 		public ?HttpJson $json = null;
 
-		/** @var HttpHeaderList|null */
-		public HttpHeaderList|null $headers = null;
-
 		/**
 		 * Request cookies
 		 * @var CookieJarInterface|null
 		 */
 		public ?CookieJarInterface $cookies = null;
-
 
 		/**
 		 * Request ssl certificate
@@ -48,15 +44,6 @@
 		 */
 		public int|float|null $delay = null;
 
-		/**
-		 * Describes the SSL certificate verification behavior of a request.
-		 *  - Set to true to enable SSL certificate verification and use the default CA bundle provided by operating system.
-		 *  - Set to false to disable certificate verification (this is insecure!).
-		 *  - Set to a string to provide the path to a CA bundle to enable verification using a custom certificate.
-		 *
-		 * @var bool|string|null
-		 */
-		public bool|string|null $verify = null;
 
 		public function getOptionsAsArray(): array {
 			$_options = [];
