a2ef240ce4f4f91ce795eb97f21c76abd0739ac1
vadim <vadimopetrov@bk.ru>
2026-07-22 22:11:20 +0500

    test(http request options): test request with json body

--- test/mrvpetrov/Http/Client/Facade/HttpClientTest.php
+++ test/mrvpetrov/Http/Client/Facade/HttpClientTest.php
@@ -5,6 +5,8 @@
 	use mrvpetrov\Http\Client\Facade\HttpClient;
 	use mrvpetrov\Http\Client\Facade\HttpClientOptions;
 	use mrvpetrov\Http\Client\Facade\HttpHeader;
+	use mrvpetrov\Http\RequestOptions\HttpJson;
+	use mrvpetrov\Http\RequestOptions\HttpRequestOptions;
 	use PHPUnit\Framework\TestCase;
 	use function PHPUnit\Framework\assertEquals;
 
@@ -23,5 +25,22 @@
 
 			assertEquals(200, $res->getStatusCode());
 			self::assertCount(100, $json);
+		}
+
+		function test_json_request() {
+			$client = new HttpClient(
+				HttpClientOptions::new()
+				->setBaseUri('https://jsonplaceholder.typicode.com/')
+				->addHeader(new HttpHeader("test", 'value'))
+				->setVerify(false)
+			);
+
+			$options = HttpRequestOptions::new()->setJson(HttpJson::fromArray(['UserId' => 1, 'title' => 'test title', 'body' => 'test body']));
+			$res = $client->post( 'posts', $options);
+
+			$json = json_decode($res->getBody()->getContents(), true);
+
+			assertEquals(201, $res->getStatusCode());
+			assertEquals(101, $json['id']);
 		}
 	}
